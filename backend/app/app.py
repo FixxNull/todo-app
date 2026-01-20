66675860e98765d3df94ad288c6f4002ce863b00
Denis Ovsiannikov <den.ovsiannikov1@gmail.com>
2026-01-20 22:23:26 +0300

    Add new user feature implementation. Added mongodb connection in application. Added user model for database collect and store data.

---
+++ backend/app/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from app.core.config import settings
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from app.models.user_model import User
+from app.api.api_v1.router import router
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+@app.on_event("startup")
+async def app_init():
+    """
+    Initialize application services like database connections.
+    """
+    # Initialize MongoDB connection
+    client = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING)
+    database = client.todo_database
+    
+    # Initialize Beanie with the database and document models
+    await init_beanie(database=database, document_models=[User])  # Add your document models here   
+
+app.include_router(router, prefix=settings.API_V1_STR)
---
+++ backend/app/services/user_service.py
@@ -0,0 +1,18 @@
+from app.schemas.user_schema import UserAuth
+from app.models.user_model import User
+from app.core.security import get_password
+
+
+class UserService:
+    @staticmethod
+    async def create_user(user: UserAuth):
+        user_in = User(
+            username=user.username,
+            email=user.email,
+            hashed_password=get_password(user.password)
+        )
+
+        await user_in.insert()
+        return user_in
+
+
